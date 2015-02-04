09cd460857e50911d5b34a7d0ae6e47c1872cbcc
a100 <a100@a100cyyLDN1.(none)>
2015-02-04 21:16:47 +0800

    add commnet in Vagrantfile

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
+
+#exit the script on error
 set -e
 
 sudo yum install -y vim
 sudo yum install -y openssl
-sudo yum install -y ntp
 sudo yum install -y wget
+
+#setting NTP
+sudo yum install -y ntp
 sudo chkconfig ntpd on
 sudo service ntpd start
 
+#copy ssh key which is generated before to root user home for ambari-server login.  
 sudo cp -r /vagrant/sshkey /root/.ssh
-sudo cp -r /vagrant/hosts /etc/hosts
 
+#use local dns
+sudo cp -r /vagrant/hosts /etc/hosts
 sudo service network restart
 
+#down load ambari-server
 sudo wget -nv http://public-repo-1.hortonworks.com/ambari/centos6/1.x/updates/1.7.0/ambari.repo -O /etc/yum.repos.d/ambari.repo
-sudo  yum install -y ambari-server
+sudo yum install -y ambari-server
